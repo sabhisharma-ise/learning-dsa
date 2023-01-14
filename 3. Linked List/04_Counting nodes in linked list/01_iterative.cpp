1e88af293f6cbbc40a4fde78e687370b053d1f08
sabhisharma-ise <2021is_sabhisharma_b@nie.ac.in>
2023-01-14 14:29:36 +0530

    Counting nodes in linked list

---
+++ 3. Linked List/04_Counting nodes in linked list/01_iterative.cpp
@@ -0,0 +1,29 @@
+// Iteratively, Counting the number of nodes in a linked list 
+// Or simply the length of the linked list
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+// Function to count the number of nodes in a linked list
+int countNode (struct Node *p) {
+    int count = 0;
+    while (p != NULL) {
+        count++;
+        p=p->next;
+    }
+}
+
+int main () {
+
+    struct Node *first;
+    first = new Node;
+
+    countNode(first);
+
+    return 0;
+}
---
+++ 3. Linked List/04_Counting nodes in linked list/02_recursive.cpp
@@ -0,0 +1,30 @@
+// Recursively, Counting the number of nodes in a linked list 
+// Or simply the length of the linked list
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+// Recursive function to count the number of nodes in a linked list
+int countNode (struct Node *p) {
+    if (p != NULL) {
+        return countNode(p->next) + 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main () {
+
+    struct Node *first;
+    first = new Node;
+
+    countNode(first);
+
+    return 0;
+}
